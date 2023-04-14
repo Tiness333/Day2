cbbcf1e0f17a716c4482cd2d7e23ce02b19ec294
Tiness333 <13111855643@163.com>
2023-04-14 09:46:35 +0800

    修改用例失败异常截图逻辑

--- Day2/day2_work.py
+++ Day2/day2_work.py
@@ -16,48 +16,48 @@ class DaytwoWork(unittest.TestCase):
     def setUp(self) -> None:
         self.driver=Search(driver)
     #关闭网页
-    # def tearDown(self) -> None:
-    #     self.driver.quit()
+    def tearDown_class(self) -> None:
+        self.driver.quit()
     #冒烟测试
     def test_case001(self):
         self.driver.search(value='selenium')
         res_slement = self.driver.text((By.CSS_SELECTOR, ".topic-title"))
-        assert 'selenium' in res_slement
         if 'selenium' in res_slement:
             pass
         else:
             self.driver.save_screenshot("冒烟失败.png")
+        assert 'selenium' in res_slement
         # allure.attach.file("image.png", name='hogwarts', attachment_type=allure.attachment_type.PNG)
 
     # 测试用例2，高级搜索测试
     def test_case002(self):
         self.driver.seniorsearch(value='selenium')
         res_slement=self.driver.text((By.CSS_SELECTOR,".topic-title"))
-        assert 'selenium' in res_slement
         if 'selenium' in res_slement:
             pass
         else:
             self.driver.save_screenshot("高级搜索失败.png")
+        assert 'selenium' in res_slement
 
     #测试用例3，搜索空内容
     def test_case003(self):
         self.driver.seniorsearch(value='')
         res_slement = self.driver.text((By.CSS_SELECTOR, ".fps-invalid"))
-        assert '您的搜索词过短。' == res_slement
         if '您的搜索词过短。' == res_slement:
             pass
         else:
             self.driver.save_screenshot("空内容搜索失败.png")
+        assert '您的搜索词过短。' == res_slement
 
     #测试用例4，特殊字符
     def test_case004(self):
         self.driver.seniorsearch(value='$*&^^')
         res_slement = self.driver.text((By.CSS_SELECTOR, ".loading-container h3"))
-        assert '找不到结果222。' == res_slement
         if '找不到结果222。' == res_slement:
             pass
         else:
             self.driver.save_screenshot("特殊字符搜索失败.png")
+        assert '找不到结果222。' == res_slement
 
 
 
